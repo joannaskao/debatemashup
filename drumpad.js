a4eb64e9d1ae5e08c52d48a0b6aba5179150957b
Joanna Kao <joannaskao@gmail.com>
2012-10-20 12:56:13 -0400

    Changed to keymapper for key events.

--- drumpad.js
+++ drumpad.js
@@ -84,6 +84,36 @@ function playForKey(code) {
 
 }
 
+// keymapper maps keys to their event code
+keymapper = {
+  65: 'a',
+  66: 'b',
+  67: 'c',
+  68: 'd',
+  69: 'e',
+  70: 'f',
+  71: 'g',
+  72: 'h',
+  73: 'i',
+  74: 'j',
+  75: 'k',
+  76: 'l',
+  77: 'm',
+  78: 'n',
+  79: 'o',
+  80: 'p',
+  81: 'q',
+  82: 'r',
+  83: 's',
+  84: 't',
+  85: 'u',
+  86: 'v',
+  87: 'w',
+  88: 'x',
+  89: 'y',
+  90: 'z',
+}
+
 function handleKeyPress(e) {
 
   if (typeof clock_start === 'undefined') {
@@ -91,13 +121,7 @@ function handleKeyPress(e) {
     metronome();
   }
 
-  switch(e.keyCode) {
-    case 65   : playForKey('a'); break;
-    case 83   : playForKey('s'); break;
-    case 68   : playForKey('d'); break;
-    case 70  : playForKey('f'); break;
-    break;
-  }
+  playForKey(keymapper[e.keyCode]);
 }
 
 function mapBufferToList(buffer, key) {
@@ -173,5 +197,7 @@ $(document).ready(function() {
       handleKeyPress(e);
     },
     false);
-  
+
+  init_interactions();
+
 });
