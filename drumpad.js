b4eec2c3882a60c451f934f233bab8a727f84d11
Joanna Kao <joannaskao@gmail.com>
2012-10-20 14:12:19 -0400

    Added spinner

--- drumpad.js
+++ drumpad.js
@@ -179,7 +179,7 @@ function preloadData() {
       mapBufferToList(list[0], 'a');
       mapBufferToList(list[1], 's');
       mapBufferToList(list[list.length-1], 'background');
-      $('.loadingscreen').remove(); // removes the loading screen
+      $('#loadingscreen').remove(); // removes the loading screen
   });
   bufferLoader.load();
   
@@ -194,8 +194,31 @@ function setup() {
   }
 }
 
+function spinner(){
+    var opts = {
+    lines: 13, // The number of lines to draw
+    length: 7, // The length of each line
+    width: 4, // The line thickness
+    radius: 10, // The radius of the inner circle
+    corners: 1, // Corner roundness (0..1)
+    rotate: 0, // The rotation offset
+    color: '#EEE', // #rgb or #rrggbb
+    speed: 1, // Rounds per second
+    trail: 60, // Afterglow percentage
+    shadow: false, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: 'auto', // Top position relative to parent in px
+    left: 'auto' // Left position relative to parent in px
+  };
+  var target = document.getElementById('loadingscreen');
+  var spinner = new Spinner(opts).spin(target);
+}
+
 $(document).ready(function() {
   // Handler for .ready() called.;
+  spinner()
   setup();
   preloadData();
 
